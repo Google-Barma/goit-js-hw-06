619ea4eb8374ba03852931f391176a4b378a2b26
Vadim Yevlanov <vad.evlanov@gmail.com>
2022-12-27 15:22:31 +0100

    Finished second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const markup = ingredients
+  .map((ingredient) => `<li class="item"><p>${ingredient}</p></li>`)
+  .join("");
+
+list.innerHTML = markup;
