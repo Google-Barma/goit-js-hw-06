b6c4e0f5c05a13da6cdeb5d5bf5d780c975b3bfe
Vadim Yevlanov <vad.evlanov@gmail.com>
2022-12-30 14:25:07 +0100

    Finished ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
-function getRandomHexColor() {
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const colorSpanEl = document.querySelector(".color");
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const changeBackgroundColor = (color) => (bodyEl.style.backgroundColor = color);
+
+const changeColorText = (color) => (colorSpanEl.textContent = color);
+
+const onPushBtn = () => {
+  const color = getRandomHexColor();
+
+  changeBackgroundColor(color);
+  changeColorText(color);
+};
+
+btnEl.addEventListener("click", onPushBtn);
